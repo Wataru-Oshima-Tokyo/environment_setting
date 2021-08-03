c05a7de9b16ad0a6d14733fb378529f5881d208f
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-08-03 11:34:12 +0900

    Update forXavier.sh

--- forXavier.sh
+++ forXavier.sh
@@ -1,46 +1,26 @@
 echo 'lcm-server'
 
 cd ~/
-
 git clone https://github.com/TechShare-inc/slamrplidar
-
 cd slamrplidar/slam_planner_sdk/Thirdparty/
-
 sudo tar -xvf lcm-1.4.0.tar.gz
-
 mv lcm-1.4.0 ~/
-
 cd ~/
-
 sudo rm -r slamrplidar
-
 cd ~/lcm-1.4.0/
-
 mkdir build && cd build
-
 cmake ..
-
 make -j4
-
 sudo make install
 
 echo 'unitree_legged_sdk'
-
 cd ~/
-
 git clone https://github.com/Wataru-Oshima-Tokyo/unitree_legged_sdk
-
 cd unitree_legged_sdk
-
 mkdir build && cd build
-
 cmake ..
-
 make -j4
-
 cd ~/
-
-
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u
 sudo apt install librealsense2-utils
